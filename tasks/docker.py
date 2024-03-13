bd07b7fa3b0f803cfc2905f6676cc605afb2f356
shaun coughlin <scoughlin@cfs.energy>
2024-03-13 10:09:12 -0400

    update 3.1.8(arn+amd) build

--- tasks/docker.py
+++ tasks/docker.py
@@ -33,19 +33,27 @@ def prepare_artifacts(c):
 def build(c, no_cache=False):
     # type: (Context, bool) -> None
     """
-    build administrator docker image
+    Build administrator Docker image for multiple architectures (AMD64 and ARM).
     """
 
     prepare_artifacts(c)
 
     release_version = idea.props.idea_release_version
-    build_cmd = str(f'docker build '
-                    f'--build-arg PUBLIC_ECR_TAG=v{release_version} '
-                    f'-t idea-administrator:v{release_version} '
-                    f'"{idea.props.deployment_administrator_dir}"')
+    build_cmd_amd = str(f'docker buildx build --platform linux/amd64 '
+                        f'--build-arg PUBLIC_ECR_TAG=v{release_version} '
+                        f'-t idea-administrator-amd64:v{release_version} '
+                        f'"{idea.props.deployment_administrator_dir}"')
+    build_cmd_arm = str(f'docker buildx build --platform linux/arm64 '
+                        f'--build-arg PUBLIC_ECR_TAG=v{release_version} '
+                        f'-t idea-administrator-arm64:v{release_version} '
+                        f'"{idea.props.deployment_administrator_dir}"')
+
     if no_cache:
-        build_cmd = f'{build_cmd} --no-cache'
-    c.run(build_cmd)
+        build_cmd_amd = f'{build_cmd_amd} --no-cache'
+        build_cmd_arm = f'{build_cmd_arm} --no-cache'
+
+    c.run(build_cmd_amd)
+    c.run(build_cmd_arm)
 
 
 def publish(c, ecr_registry, ecr_tag):
